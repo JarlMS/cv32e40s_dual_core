eb859e184d65283fd8870babbf035e5f4de5cd67
JarlMS <jarlmag@gmail.com>
2023-11-30 15:20:22 +0100

    syntax

--- rtl/cv32e40s_glitch_injector.sv
+++ rtl/cv32e40s_glitch_injector.sv
@@ -9,13 +9,13 @@ module cv32e40s_glitch_injector #(parameter BIT_LENGTH = 1, parameter SPECIFIC =
     output  logic [0:BIT_LENGTH-1]     out,
     input   logic [0:BIT_LENGTH-1]     in,
     input   wire                    clk,
-    input   wire                    reset,
+    input   wire                    rst_n,
     input   wire                    enable,
     input   wire                    enable_specific    
 );
 
-always_ff @ (posedge clk or negedge reset) begin 
-    if (!reset) begin 
+always_ff @ (posedge clk, negedge rst_n) begin 
+    if (rst_n == 1'b0) begin 
         out <= 0;
     end else begin
         reg [BIT_LENGTH-1:0] random_out; // Declare 'random_out' as a register
